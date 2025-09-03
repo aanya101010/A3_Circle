7a060967eae54d0269f16dc9fde8a75420b7add5
aanya101010 <aanya.singh901@gmail.com>
2025-09-02 19:19:32 -0500

    A3_Circle.java

--- A3_Circle.java
+++ A3_Circle.java
@@ -4,8 +4,8 @@ public class A3_Circle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt for radius
-        System.out.println("Enter the radius of your circle:");
+        // Prompt
+        System.out.println("Enter the radius of your circle: ");
         double radius = scanner.nextDouble();
 
         // Calculations
@@ -15,9 +15,9 @@ public class A3_Circle {
         // Output
         System.out.println();
         System.out.println("Circle Properties:");
-        System.out.printf("%-13s %10.3f%n", "Radius:", radius);
-        System.out.printf("%-13s %10.3f%n", "Circumference:", circumference);
-        System.out.printf("%-13s %10.3f%n", "Area:", area);
+        System.out.printf("%-14s %10.3f%n", "Radius: ", radius);
+        System.out.printf("%-14s %10.3f%n", "Circumference:", circumference);
+        System.out.printf("%-14s %10.3f%n", "Area: ", area);
 
         scanner.close();
     }
